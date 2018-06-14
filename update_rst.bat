1c4223e82a756b16d6405edabda7dda8dfab4989
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-06-15 00:59:57 +0700

    commit

--- update_rst.bat
+++ update_rst.bat
@@ -3,3 +3,4 @@ pandoc README.md -o README.rst
 sphinx-build -b rst docs docs\_build
 ::sphinx-apidoc -f -o docs\_build docs
 copy docs\_build\api.rst api.rst
+@echo replace '         *' -^> '          *' manually
