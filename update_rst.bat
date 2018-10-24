dfa4354fa5b8894350c36831062cd16fa1218e72
Peter Zagubisalo <peter.zagubisalo@gmail.com>
2018-10-24 23:22:42 +0700

    Update update_rst.bat

--- update_rst.bat
+++ update_rst.bat
@@ -1,8 +1,8 @@
-set "script_dir=%~dp0"
-cd /d "%script_dir%"
+cd /d "%~dp0"
 
-chcp 65001 > NUL
 pandoc README.md -o README.rst
 sphinx-build -b rst docs docs\_build
+chcp 65001
 type docs\_build\api.rst | python docs/rst_fix.py > api.rst
+chcp 1252
 rmdir /s /q docs\_build
