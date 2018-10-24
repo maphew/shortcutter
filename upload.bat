f2462e09be4b0cd8659a9c304ca7ccb54744b600
Peter Zagubisalo <peter.zagubisalo@gmail.com>
2018-10-24 23:14:55 +0700

    Update upload.bat

--- upload.bat
+++ upload.bat
@@ -2,6 +2,4 @@ set "script_dir=%~dp0"
 cd /d "%script_dir%"
 
 python setup.py sdist
-chcp 1252 && set "PYTHONIOENCODING="
 twine upload dist/* --skip-existing
-chcp 65001 && set "PYTHONIOENCODING=utf-8"
