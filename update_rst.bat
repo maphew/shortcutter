66aaa3f2ec2110aa752f985c1601a66a099deef1
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-12-12 15:15:39 +0700

    removed batch scripts

--- update_rst.bat
+++
@@ -1,8 +0,0 @@
-cd /d "%~dp0"
-
-pandoc README.md -o README.rst
-sphinx-build -b rst docs docs\_build
-chcp 65001
-type docs\_build\api.rst | python docs/rst_fix.py > api.rst
-chcp 1252
-rmdir /s /q docs\_build
